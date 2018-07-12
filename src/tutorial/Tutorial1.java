bd7eaa9056ff15d8aeff6e722c0238c1b4e8f526
meisam <meisam@wirelessprv-10-194-99-43.near.illinois.edu>
2018-07-12 10:13:33 -0500

    minor changes to Tutorial1

--- src/tutorial/Tutorial1.java
+++ src/tutorial/Tutorial1.java
@@ -2,11 +2,12 @@ package tutorial;
 
 import plotter.MatlabChart;
 import util.MoreMath;
+import static java.lang.Math.*;
 
 public class Tutorial1 {
 	public static void main(String[] args) {
 		//1. define the x and y variables
-		double[] x = MoreMath.linspace(-3*Math.PI, 3*Math.PI, 1000) ;
+		double[] x = MoreMath.linspace(-3*PI, 3*PI, 1000) ;
 		double[] y = MoreMath.Arrays.Functions.sin(x) ;
 		//2. create the matlab chart using jfreechart
 		MatlabChart fig = new MatlabChart() ;
