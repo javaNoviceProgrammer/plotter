adbec0e7cfb11cccf44147f8f456c8276b68e354
ilirm <ilirm@ilirm-PC>
2018-07-13 10:45:51 -0500

    more

--- src/tutorial/Tutorial0.java
+++ src/tutorial/Tutorial0.java
@@ -1,4 +1,4 @@
-package plotter.tutorial;
+package tutorial;
 
 public class Tutorial0 {
 	public static void main(String[] args) {
--- src/tutorial/Tutorial1.java
+++ src/tutorial/Tutorial1.java
@@ -1,4 +1,4 @@
-package plotter.tutorial;
+package tutorial;
 
 import static java.lang.Math.*;
 
