4c5b8d6af32afbd07dc69c34cbea61dbfd6d37bd
javaNoviceProgrammer <40440007+javaNoviceProgrammer@users.noreply.github.com>
2018-08-02 08:29:07 -0500

    minor change.

--- src/tutorial/Tutorial1.java
+++ src/tutorial/Tutorial1.java
@@ -5,28 +5,27 @@ import static java.lang.Math.PI;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
-import com.sun.java.swing.plaf.windows.WindowsLookAndFeel;
-
 import plotter.chart.MatlabChart;
 import plotter.util.MoreMath;
 
 public class Tutorial1 {
-	public static void main(String[] args) throws UnsupportedLookAndFeelException {
+	public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException,
+			InstantiationException, IllegalAccessException {
 		// 0. setting the UI look and feel
-		UIManager.setLookAndFeel(new WindowsLookAndFeel());
-		//1. define the x and y variables
-		double[] x = MoreMath.linspace(-3*PI, 3*PI, 1000) ;
-		double[] y = MoreMath.Arrays.Functions.sin(x) ;
-		//2. create the matlab chart using jfreechart
-		MatlabChart fig = new MatlabChart() ;
-		//3. add as many variables to the figure
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		// 1. define the x and y variables
+		double[] x = MoreMath.linspace(-3 * PI, 3 * PI, 1000);
+		double[] y = MoreMath.Arrays.Functions.sin(x);
+		// 2. create the matlab chart using jfreechart
+		MatlabChart fig = new MatlabChart();
+		// 3. add as many variables to the figure
 		fig.plot(x, y);
-		//4. after adding the plots, render the chart
+		// 4. after adding the plots, render the chart
 		fig.RenderPlot();
-		//5. set the x and y lables
+		// 5. set the x and y lables
 		fig.xlabel("X variable");
 		fig.ylabel("Y variable");
-		//6. show the plot
+		// 6. show the plot
 		fig.run(true);
 	}
 }
